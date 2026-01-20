e4d1c391f204afe48a9279ee93a99fd776b17476
Mikel <mihailpanukov711@yandex.ru>
2026-01-20 20:50:19 +0400

    add logger seting

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-
+from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 
 from fastapi.middleware.cors import CORSMiddleware
@@ -9,7 +9,12 @@ from fastapi.middleware.gzip import GZipMiddleware
 
 from app.routers import categories,products, users, reviews, cart, orders, payments, products_v2
 from app.routers.statistics import products_stats,category_stats
-import time
+
+from time import time
+from loguru import logger
+from uuid import uuid4
+
+logger.add("info.log", format="Log: [{extra[log_id]}:{time} - {level} - {message}]", level="INFO", enqueue = True)
 
 
 
@@ -20,6 +25,22 @@ app = FastAPI(
     version="1.0"
 )
 
+@app.middleware("http")
+async def log_middleware(request: Request, call_next):
+    log_id = str(uuid4())
+    with logger.contextualize(log_id=log_id):
+        try:
+            response = await call_next(request)
+            if response.status_code in [401, 402, 403, 404]:
+                logger.warning(f"Request to {request.url.path} failed")
+            else:
+                logger.info('Successfully accessed ' + request.url.path)
+        except Exception as ex:
+            logger.error(f"Request to {request.url.path} failed: {ex}")
+            response = JSONResponse(content={"success": False}, status_code=500)
+        return response
+
+
 
 app.add_middleware( #4
     GZipMiddleware,
@@ -29,9 +50,9 @@ app.add_middleware( #4
 
 @app.middleware("http") #3
 async def modify_request_response_middleware(request: Request, call_next):
-    start_time = time.time() #текущее время
+    start_time = time() #текущее время
     response = await call_next(request) #некст мидлвар или приложение 
-    duration = time.time() - start_time #смотрим время
+    duration = time() - start_time #смотрим время
     print(f"Request duration: {duration:.10f} seconds") #логирование
     return response  #возврат запроса
 
@@ -46,12 +67,12 @@ app.add_middleware( #2
 app.add_middleware( #1
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",           # React
-        "http://localhost:5173",           # Vite
+        "http://localhost:3000",           
+        "http://localhost:5173",           
     ],
-    allow_credentials=True, #разрешены ли заголовки и куки в запросах
-    allow_methods=["*"], #все методы
-    allow_headers=["Authorization", "Content-Type"], #какие заголовки можно в ручную указывать
+    allow_credentials=True,  
+    allow_methods=["*"],  
+    allow_headers=["Authorization", "Content-Type"] 
 )
  
 
